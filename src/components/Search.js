893f058603895e585a016083632dfd0f7e217164
Girish2003 <cgirish2003@gmail.com>
2024-01-15 10:18:07 +0530

    fourth

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,32 +1,83 @@
-import React, { useState } from "react";
-import searchIcon from '../assets/search-icon.svg'
-const Search = () => {
-    const [searchText, setsearchText] = useState("");
-    let handleInput=(e)=>{
-        e.preventDefault();
-        let query=e.target.value;
-        setsearchText(query)
-    }
+import React, { useContext, useState } from "react";
+import searchIcon from "../assets/search-icon.svg";
+import { CryptoContext } from "../context/CryptoContext";
+import debounce from "lodash.debounce";
+
+const SearchInput = ({ handleSearch }) => {
+  const [searchText, setSearchText] = useState("");
+  let { searchData } = useContext(CryptoContext);
+
+  let handleInput = (e) => {
+    e.preventDefault();
+    let query = e.target.value;
+    setSearchText(query);
+    handleSearch(query);
+  };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   handleSearch(searchText);
+  // };
+
+  // const selectCoin = (coin) => {
+  //   setCoinSearch(coin);
+  //   setSearchText("");
+  //   setSearchData();
+  // };
+
   return (
-    <form
-      className="w-96 relative flex items-center
-ml-7 font-nunito
-"
-    //   onSubmit={handleSubmit}
-    >
-      <input
-        type="text"
-        name="search"
-        onChange={handleInput}
-        value={searchText}
-        className="w-full rounded bg-gray-200 placeholder:text-gray-100 pl-2 required outline-0 border border-transparent focus:border-cyan
-     "
-        placeholder="search here..."
-      />
-      <button type="submit" className="absolute right-1 cursor-pointer">
-        <img src={searchIcon} className="w-full h-auto" alt="search" />
-      </button>
-    </form>
+    <>
+      <form
+        className="w-96 relative flex items-center
+    ml-7 font-nunito
+    "
+        // onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          name="search"
+          onChange={handleInput}
+          value={searchText}
+          className="w-full rounded bg-gray-200
+        placeholder:text-gray-100 pl-2
+        required outline-0 border border-transparent 
+        focus:border-cyan
+         "
+          placeholder="search here..."
+        />
+        <button type="submit" className="absolute right-1 cursor-pointer">
+          <img src={searchIcon} className="w-full h-auto" alt="search" />
+        </button>
+      </form>
+      {searchText.length > 0 ? (
+        <ul
+          className="absolute top-11 right-0 py-2 w-full h-96 rounded overflow-x-hidden
+      bg-gray-200 bg-opacity-60 backdrop-blur-md"
+        >
+          {searchData ? (
+            searchData.map((coin) => {
+              return <li>{coin.id}</li>;
+            })
+          ) : (
+            <h2>Please wait...</h2>
+          )}
+        </ul>
+      ) : null}
+    </>
+  );
+};
+
+const Search = () => {
+  let { getSearchResult } = useContext(CryptoContext);
+
+  const debounceFunc = debounce(function (val) {
+    getSearchResult(val);
+  }, 2000);
+
+  return (
+    <div className="relative">
+      <SearchInput handleSearch={debounceFunc} />
+    </div>
   );
 };
 
--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -1,37 +1,49 @@
 import { createContext, useLayoutEffect, useState } from "react";
 
-export const CryptoContext=createContext({});
-export const CryptoProvider=({children})=>{
-    const [cryptoData, setcryptoData] = useState()
+export const CryptoContext = createContext({});
+export const CryptoProvider = ({ children }) => {
+  const [cryptoData, setcryptoData] = useState();
+  const [searchData, setSearchData] = useState([]);
 
-    // const getCryptoData = async () => {
-      
-        
-    
+  const getCryptoData = async () => {
+    try {
+      const data =
+        await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en
 
 
+            `)
+          .then((res) => res.json())
+          .then((json) => json);
 
-    const getCryptoData=async()=>{
-        try{
-            const data=await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en
-
-            `).then(res=>res.json()).then(json=>json);
-
-            console.log(data);
-            setcryptoData(data);    
-
-        }
-        catch(error){
-            console.log(error);
-        }
+      setcryptoData(data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useLayoutEffect(() => {
-      getCryptoData();
-    }, [])
-    return(
-        <CryptoContext.Provider value={{cryptoData}}>
-            {children}
-        </CryptoContext.Provider>
-    )
-}
+  const getSearchResult = async (query) => {
+    try {
+      const data =
+        await fetch(`https://api.coingecko.com/api/v3/search?query=${query}
+
+
+            `)
+          .then((res) => res.json())
+          .then((json) => json);
+
+      console.log(data);
+      setSearchData(data.coins);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useLayoutEffect(() => {
+    getCryptoData();
+  }, []);
+  return (
+    <CryptoContext.Provider value={{ cryptoData, searchData, getSearchResult }}>
+      {children}
+    </CryptoContext.Provider>
+  );
+};
